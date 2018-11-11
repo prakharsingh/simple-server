d85c161dda83da2267e6a83be73dfee058e3c924
Prakhar Singh <prakhar.singh007@gmail.com>
2018-11-11 19:57:28 +0530

    hapi server with aws s3 pre-signed url

--- src/aws/schemas/upload-file.js
+++ src/aws/schemas/upload-file.js
@@ -4,7 +4,15 @@ const S3Object = require("../constants");
 module.exports = Joi
     .object()
     .keys({
-        Bucket: Joi.string().required(),
-        Key: Joi.string().required(),
-        ACL: Joi.string().valid(S3Object.S3ObjectAcl).required(),
+      bucket          : Joi.string().required(),
+      key             : Joi.string().required(),
+      maxSize         : Joi.number().required(),
+      expireInHour    : Joi.alternatives().when(
+        'expireInMin',
+        {
+          is: true, then: Joi.any().optional(),
+          otherwise: Joi.number().required()
+        }),
+      expireInMin     : Joi.number().optional(),
+      acl             : Joi.string().valid(S3Object.S3ObjectAcl).required()
     });
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,1 +1,49 @@
-module.exports = [];
+const Joi = require('joi');
+
+module.exports = [
+  {
+    path: "/api/submit",
+    method: "POST",
+    config: {
+      validate: {
+        payload: {
+          category: Joi.string(),
+          categoryType: Joi.string().allow(['new', 'existing']),
+          busNameEng: Joi.string(),
+          busNameArab: Joi.string(),
+          branchNameEng: Joi.string(),
+          branchNameArab: Joi.string(),
+          location: Joi.string().required(),
+          city: Joi.string().required(),
+          country: Joi.string().required(),
+          instaUrl: Joi.string(),
+          fbUrl: Joi.string(),
+          about: Joi.string(),
+          email: Joi.string().email(),
+          phone: Joi.string(),
+          membership: Joi.string().allow(['category1', 'category2']).required(),
+          profilePicUrl: Joi.string(),
+        },
+        failAction: async (request, h, err) => {
+          if (process.env.NODE_ENV === 'production') {
+            // In prod, log a limited error message and throw the default Bad Request error.
+            console.error('ValidationError:', err.message);
+            throw Boom.badRequest(`Invalid request payload input`);
+          } else {
+            // During development, log and respond with the full error.
+            console.error(err);
+            throw err;
+          }
+        }
+      },
+      handler: async (request) => {
+        try{
+          return request.payload;
+        } catch(error) {
+          request.server.log('error', '---UNHANDLED ERROR---' + error.toString());
+          return Boom.internal();
+        }
+      }
+    }
+  }
+];
